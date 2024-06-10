839679817834f10cdd82adb52a94a8cd0206aee5
JeeSooPark <76983198+BeTheWings@users.noreply.github.com>
2024-06-10 13:15:45 +0900

    Slide효과 및 이미지 fill추가
    1. slide.css에 애니메이션 효과를 추가하여 slide시 강조를 하도록 변경
    2. slide.css에서 이미지를 넣었을 경우 fill을 통해서 이미지 크기를 div의 slide-item에 맞추어 조절할 수 있도록 변경
    3.test2.jpg 추가

--- wanted/src/main/front/src/layout/Sliders.js
+++ wanted/src/main/front/src/layout/Sliders.js
@@ -2,32 +2,33 @@ import React, {Component} from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import logo from "../logo.svg";
+import logo from "../image/logo.svg";
+import testImg from "../image/test2.jpg"
 import "../css/sliders.css";
 
 export default class Sliders extends Component{
     render() {
         const setting={
-            dots:false,
+            dots:true,
             infinite:true,
             speed:500,
             slidesToShow:1,
             slidesToScroll:1,
+            autoplay:true,
+            autoplaySpeed:3000,
+            cssEase:'ease-in-out'
         };
         return(
             <div className="sliders-container">
                 <Slider {...setting}>
                     <div className="slider-item">
                         <img src={logo} className="slider-image" alt="logo"/>
-                        <h3>1</h3>
                     </div>
-                    <div>
-                        <img src={logo} className="slider-image" alt="logo"/>
-                        <h3>2</h3>
+                    <div className="slider-item">
+                        <img src={testImg} className="slider-image-size" alt="testImg"/>
                     </div>
-                    <div>
+                    <div className="slider-item">
                         <img src={logo} className="slider-image" alt="logo"/>
-                        <h3>3</h3>
                     </div>
                 </Slider>
             </div>
